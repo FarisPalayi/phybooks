ef428856b4ba27910a8545cf8b7e7c483e80d9e4
FarisPalayi <farispalayi@gmail.com>
2024-01-19 18:33:36 +0530

    header base styles

--- src/app/components/Header.js
+++ src/app/components/Header.js
@@ -1,57 +1,49 @@
 import Image from "next/image";
-import styles from "../styles/components/Header.module.scss";
+import header from "../styles/components/Header.module.scss";
 
 export default function Header() {
   return (
-    <header className={styles.header}>
-      <a href="#" className="logo">
+    <header className={header.header}>
+      <a href="#" className={header.logo}>
         <Image src="/images/logo.svg" alt="" width={130} height={25} priority />
       </a>
 
-      <button className="search">
-        <Image src="/images/search.svg" alt="" width={24} height={24} />
-      </button>
+      <div className={header.iconContainer}>
+        <button className="">
+          <Image src="/images/search.svg" alt="" width={24} height={24} />
+        </button>
+        <button className="">
+          <Image src="/images/menu.svg" alt="" width={30} height={30} />
+        </button>
+      </div>
 
-      <button className="menu">
-        <Image src="/images/menu.svg" alt="" width={30} height={30} />
-      </button>
-
-      <nav className="nav">
-        <ul className="nav__list">
-          <li className="nav__item">Favorites</li>
-          <li className="nav__item">
-            <button
-              className="nav__toggle-btn"
-              data-nav-toggle
-              data-arrow="down"
-            >
-              Settings
-            </button>
-            <ul className="nav__sublist hide">
-              <li className="nav__subitem">
-                <a href="#" className="nav__link">
-                  Theme
-                </a>
-              </li>
-              <li className="nav__subitem">
-                <a href="#" className="nav__link">
-                  UI
-                </a>
-              </li>
-            </ul>
-          </li>
-
-          <li className="nav__item">
-            <a href="#">Contact Us</a>
-          </li>
-          <li className="nav__item">
-            <a href="#">Source Code</a>
-          </li>
-          <li className="nav__item">
-            <a href="#">About</a>
-          </li>
-        </ul>
-      </nav>
+      <div className={header.sidebarHidden}>
+        <nav className="sidebar__nav">
+          <ul className="sidebar__nav-list">
+            <li className="sidebar__nav-item">Favorites</li>
+            <li className="sidebar__nav-item">
+              <button className="sidebar__nav__toggle-btn">Settings</button>
+              <ul className="sidebar__nav__sublist hide">
+                <li className="sidebar__nav__subitem">
+                  <a href="#" className="sidebar__nav__link">Theme</a>
+                </li>
+                <li className="sidebar__nav__subitem">
+                  <a href="#" className="sidebar__nav__link">UI</a>
+                </li>
+              </ul>
+            </li>
+            <li className="sidebar__nav__item">
+              <a href="#">Contact Us</a>
+            </li>
+            <li className="sidebar__nav__item">
+              <a href="#">Source Code</a>
+            </li>
+            <li className="sidebar__nav__item">
+              <a href="#">About</a>
+            </li>
+          </ul>
+        </nav>
+      </div>
     </header>
   );
 }
