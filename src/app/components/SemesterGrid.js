75e1384aa76fef761ee35d78db30343ab0eddca3
FarisPalayi <farispalayi@gmail.com>
2024-01-24 00:14:35 +0530

    add semester section

--- src/app/components/SemesterGrid.js
+++ src/app/components/SemesterGrid.js
@@ -1,5 +1,21 @@
 import React from "react";
-export function SemesterGrid({}) {
-  return <section className="semesterGrid"></section>;
+import styles from "../styles/components/SemesterGrid.module.scss";
+
+export function SemesterGrid() {
+  const semesterInRoman = ["I", "II", "III", "IV", "V", "VI"];
+
+  return (
+    <section className={styles.semester}>
+      <h2 className={styles.semesterTitle}>
+        Browse by <span>Semester</span>
+      </h2>
+      <div className={styles.semesterGrid}>
+        {semesterInRoman.map((semNum, i) => (
+          <a href="#" className={styles.gridItem} key={i}>
+            Semester {semNum}
+          </a>
+        ))}
+      </div>
+    </section>
+  );
 }
-  
