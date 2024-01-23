ef0cc8c2231b51c0fcc9c0e2feee369aae3dfbcc
FarisPalayi <farispalayi@gmail.com>
2024-01-23 22:14:55 +0530

    add default btnType

--- src/app/components/Button.js
+++ src/app/components/Button.js
@@ -1,6 +1,8 @@
 import styles from "../styles/components/Button.module.scss";
 
 export default function Button({ btnText, btnType, btnLink }) {
+  !["primary", "secondary"].includes(btnType) && (btnType = "secondary");
+
   return (
     <a href={btnLink} className={`${styles.btn} ${styles[btnType]}`}>
       {btnText}
