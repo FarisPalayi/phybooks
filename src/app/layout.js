95115aa15f620d8d7418e20aded6fd8eca32698f
FarisPalayi <farispalayi@gmail.com>
2024-01-23 22:15:06 +0530

    add theme-color meta tag

--- src/app/layout.js
+++ src/app/layout.js
@@ -3,10 +3,15 @@ import "./styles/reset.scss";
 import "./styles/global.scss";
 
 const inter = Inter({ subsets: ["latin"] });
-
+ 
 export const metadata = {
   title: "PhyBooks",
   description: "Calicut university B.Sc physics textbooks",
+};
+
+export const viewport = {
+  themeColor: "#0c122c",
+  appleMObileWebAppStatusBarStyle: "#0c122c",
 };
 
 export default function RootLayout({ children }) {
