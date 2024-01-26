57d8a828535739e701b8c4daae27d2e8e0e26a96
FarisPalayi <farispalayi@gmail.com>
2024-01-26 23:59:02 +0530

    make header fixed on top

--- src/app/books/page.js
+++ src/app/books/page.js
@@ -1,7 +1,9 @@
 export default function Page() {
+  const category = "Semester VI";
+
   return (
     <main>
-      <h1>Textbooks</h1>
+      <h1 style={{ marginTop: "76px" }}> {category} Textbooks</h1>
     </main>
   );
 }
