cf36d692650ecd586b48eb780cfc72c46ed0fcbe
FarisPalayi <farispalayi@gmail.com>
2024-01-26 16:17:41 +0530

    add footr bg proto

---
+++ src/app/components/Footer.js
@@ -0,0 +1,6 @@
+import Image from "next/image";
+import styles from "../styles/components/Footer.module.scss";
+
+export default function Footer() {
+  return <footer className={styles.footer}></footer>;
+}
--- src/app/page.js
+++ src/app/page.js
@@ -1,12 +1,13 @@
-import styles from "./page.module.css";
 import Header from "./components/Header";
 import Main from "./components/Main";
+import Footer from "./components/Footer";
 
 export default function Home() {
   return (
     <>
       <Header />
       <Main />
+      <Footer />
     </>
   );
 }
