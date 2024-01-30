05187f171c307e436b95705c00d1d2db13b1bebe
FarisPalayi <farispalayi@gmail.com>
2024-01-31 00:07:49 +0530

    add placeholder text for books page

--- src/app/books/page.js
+++ src/app/books/page.js
@@ -4,9 +4,26 @@ export default function Page() {
   const category = "Semester VI";
 
   return (
-    <main>
+    <main className="main" style={{ gap: 60 }}>
       <h1 style={{ marginTop: "76px" }}> {category} Textbooks</h1>
       <Breadcrumb />
+      <div>
+        <p style={{ marginBottom: 30 }}>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Quas dolores
+          deleniti sint earum architecto totam libero facilis esse fuga nisi,
+          assumenda autem porro harum impedit dolorum tempora consequuntur
+          excepturi? Tempore totam corporis iusto libero sapiente? Hic pariatur
+          harum necessitatibus. Porro ipsam nostrum
+        </p>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Quas dolores
+          deleniti sint earum architecto totam libero facilis esse fuga nisi,
+          assumenda autem porro harum impedit dolorum tempora consequuntur
+          excepturi? Tempore totam corporis iusto libero sapiente? Hic pariatur
+          harum necessitatibus. Porro ipsam nostrum earum ullam libero
+          doloremque culpa accusantium
+        </p>
+      </div>
     </main>
   );
 }
--- src/app/components/Main.js
+++ src/app/components/Main.js
@@ -1,12 +1,12 @@
 import SemesterGrid from "./SemesterGrid";
-import styles from "../styles/components/Main.module.scss";
+// import styles from "../styles/components/Main.module.scss";
 import Intro from "./Intro.js";
 import Subject from "./Subject.js";
 import CatalogLink from "./CatalogLink";
 
 export default function Main() {
   return (
-    <main className={styles.main}>
+    <main className="main">
       <Intro />
       <SemesterGrid />
       <Subject />
