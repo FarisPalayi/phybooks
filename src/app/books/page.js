1563988d01bd981203dadc54ba1100cdec60e2c5
FarisPalayi <farispalayi@gmail.com>
2024-01-31 17:15:23 +0530

    add lyrics component

--- src/app/books/page.js
+++ src/app/books/page.js
@@ -1,4 +1,5 @@
 import Breadcrumb from "../components/shared/Breadcrumb";
+import Lyrics from "./lyrics";
 
 export default function Page() {
   const category = "Semester 5";
@@ -7,23 +8,7 @@ export default function Page() {
     <main className="main" style={{ gap: 60 }}>
       <h1 style={{ marginTop: "76px" }}> {category} Textbooks</h1>
       <Breadcrumb />
-      <div>
-        <p style={{ marginBottom: 30 }}>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Quas dolores
-          deleniti sint earum architecto totam libero facilis esse fuga nisi,
-          assumenda autem porro harum impedit dolorum tempora consequuntur
-          excepturi? Tempore totam corporis iusto libero sapiente? Hic pariatur
-          harum necessitatibus. Porro ipsam nostrum
-        </p>
-        <p>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Quas dolores
-          deleniti sint earum architecto totam libero facilis esse fuga nisi,
-          assumenda autem porro harum impedit dolorum tempora consequuntur
-          excepturi? Tempore totam corporis iusto libero sapiente? Hic pariatur
-          harum necessitatibus. Porro ipsam nostrum earum ullam libero
-          doloremque culpa accusantium
-        </p>
-      </div>
+      <Lyrics />
     </main>
   );
 }
