a5b896cee0a6c47e9150c7ac5d064ee7bdfa7a07
FarisPalayi <farispalayi@gmail.com>
2024-01-31 17:43:13 +0530

    books page padding increased

--- src/app/books/page.js
+++ src/app/books/page.js
@@ -5,7 +5,7 @@ export default function Page() {
   const category = "Semester 5";
 
   return (
-    <main className="main" style={{ gap: 60 }}>
+    <main className="main" style={{ gap: 60, padding: "0 5px" }}>
       <h1 style={{ marginTop: "76px" }}> {category} Textbooks</h1>
       <Breadcrumb />
       <Lyrics />
