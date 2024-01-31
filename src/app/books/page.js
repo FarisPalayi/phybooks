c61938787a815579e122672e32e6523620f100d1
FarisPalayi <farispalayi@gmail.com>
2024-01-31 18:18:40 +0530

    books page spacing improved

--- src/app/books/page.js
+++ src/app/books/page.js
@@ -1,12 +1,13 @@
 import Breadcrumb from "../components/shared/Breadcrumb";
 import Lyrics from "./lyrics";
+import styles from "../styles/components/Books.module.scss";
 
 export default function Page() {
   const category = "Semester 5";
 
   return (
-    <main className="main" style={{ gap: 60, padding: "0 5px" }}>
-      <h1 style={{ marginTop: "76px" }}> {category} Textbooks</h1>
+    <main className={`main ${styles.books}`}>
+      <h1 className={styles.books__header}> {category} Textbooks</h1>
       <Breadcrumb />
       <Lyrics />
     </main>
