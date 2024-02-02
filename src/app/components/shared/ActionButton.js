4d6df3405b8f0445c6dd7e5fac4f27c382c89fb8
FarisPalayi <farispalayi@gmail.com>
2024-02-03 02:04:14 +0530

    minor changes

--- src/app/components/shared/ActionButton.js
+++ src/app/components/shared/ActionButton.js
@@ -3,13 +3,14 @@ import styles from "../../styles/components/ActionButton.module.scss";
 import Image from "next/image";
 
 export default function ActionButton({ variant, text }) {
+  console.log(variant);
   const iconPath =
     variant === "read" ? "images/book-open.svg" : "images/download.svg";
 
   return (
     <Link href="#" className={styles.btn + " " + styles[variant]}>
       <Image src={iconPath} alt="" width={12} height={12} />
-      {text}
+      <span>{text}</span>
     </Link>
   );
 }
