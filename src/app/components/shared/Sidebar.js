d02ff8e91d0a7234849250bf9cea6d5f53276627
FarisPalayi <farispalayi@gmail.com>
2024-02-05 22:39:24 +0530

    extract sidebar styles to its own file

--- src/app/components/shared/Sidebar.js
+++ src/app/components/shared/Sidebar.js
@@ -1,5 +1,5 @@
 import Link from "next/link";
-import styles from "../../styles/components/Header.module.scss";
+import styles from "../../styles/components/Sidebar.module.scss";
 
 export default function Sidebar({ isSidebarOpen }) {
   const navItems = [
