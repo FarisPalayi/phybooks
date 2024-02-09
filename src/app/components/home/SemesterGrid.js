0215f27e5f05c1737672045b1536eeda86e3c1d3
FarisPalayi <farispalayi@gmail.com>
2024-02-09 20:50:16 +0530

    minor changes

--- src/app/components/home/SemesterGrid.js
+++ src/app/components/home/SemesterGrid.js
@@ -1,4 +1,3 @@
-import Link from "next/link";
 import styles from "../../styles/components/SemesterGrid.module.scss";
 import LinkCell from "../shared/LinkCell";
 
--- src/app/components/home/Subject.js
+++ src/app/components/home/Subject.js
@@ -1,4 +1,3 @@
-import Link from "next/link";
 import styles from "../../styles/components/Subject.module.scss";
 import LinkCell from "../shared/LinkCell";
 
