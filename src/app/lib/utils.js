20280df8c077f938077ac462c00d30468495fa9d
FarisPalayi <farispalayi@gmail.com>
2024-02-25 03:49:43 +0530

    add `getBooksBySubject` util function

--- src/app/lib/utils.js
+++ src/app/lib/utils.js
@@ -6,10 +6,14 @@ function getBooksBySemester(books, semester) {
   return books.filter((book) => book.semester === semester);
 }
 
+function getBooksBuySubject(books, subject) {
+  return books.filter((book) => book.subject.includes(subject));
+}
+
 function title() {
   return this.charAt(0).toUpperCase() + this.slice(1);
 }
 
 String.prototype.title = title; // monkey patching
 
-export { setCharLimit, getBooksBySemester };
+export { setCharLimit, getBooksBySemester, getBooksBuySubject };
