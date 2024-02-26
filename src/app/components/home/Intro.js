5463fefad8a530ba40a0fd2144c2a2a22fbd2c88
FarisPalayi <farispalayi@gmail.com>
2024-02-26 20:32:30 +0530

    add intro header animation

--- src/app/components/home/Intro.js
+++ src/app/components/home/Intro.js
@@ -6,14 +6,22 @@ export default function Intro() {
   return (
     <section className={styles.intro}>
       <h1 className={styles.intro__title}>
-        <span className={styles.intro__titlePart1}>Explore.</span>
-        <span className={styles.intro__titlePart2}>Read.</span>
-        <span className={styles.intro__titlePart3}>Learn.</span>
+        <span className={styles.intro__titlePart1} data-content="Explore.">
+          Explore.
+        </span>
+        <span className={styles.intro__titlePart2} data-content="Read.">
+          Read.
+        </span>
+        <span className={styles.intro__titlePart3} data-content="Learn.">
+          Learn.
+        </span>
       </h1>
+
       <p className={styles.intro__description}>
         PhyBooks gives access to a selection of core B.Sc Physics textbooks, all
         tailored to support your academic success at Calicut University.
       </p>
+
       <div className={styles.intro__btnGroup}>
         <Button
           btnText="Browse by subject"
