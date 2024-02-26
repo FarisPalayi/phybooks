ef4a9f7b34299182a17e4a1f0548dd3703c711ca
FarisPalayi <farispalayi@gmail.com>
2024-02-26 22:14:08 +0530

    add intro page

---
+++ src/app/catalog/page.js
@@ -0,0 +1,7 @@
+export default function Page() {
+  return (
+    <main className="main">
+      <h1>Catalog</h1>
+    </main>
+  );
+}
--- src/app/components/shared/CatalogLink.js
+++ src/app/components/shared/CatalogLink.js
@@ -5,7 +5,7 @@ import Link from "next/link";
 export default function CatalogLink() {
   return (
     <div className={styles.catalog}>
-      <Link href="#" className={styles.catalog__link}>
+      <Link href="/catalog" className={styles.catalog__link}>
         <span className={styles.bookIcon}>
           <Image src={"/images/book.svg"} width={13} height={13} alt="" />
         </span>
