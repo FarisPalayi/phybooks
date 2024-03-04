c75007f29d200710905e379f199ac94d33e3b79d
FarisPalayi <farispalayi@gmail.com>
2024-03-05 00:39:53 +0530

    add cta btn to semester section

--- src/app/semester/[semNumber]/page.js
+++ src/app/semester/[semNumber]/page.js
@@ -4,6 +4,7 @@ import styles from "../../styles/components/Books.module.scss";
 import DarkCard from "../../components/books/DarkCard";
 import { textbooks } from "@/app/lib/data";
 import { getBooksBySemester } from "@/app/lib/utils";
+import Button from "@/app/components/shared/Button";
 
 export default function Page({ params }) {
   const semesterNumber = params.semNumber;
@@ -26,7 +27,26 @@ export default function Page({ params }) {
               <DarkCard key={book.id} book={book} />
             ))
           ) : (
-            <h1>Sorry, Not data found</h1>
+            <>
+              <h1>Sorry, No data is found</h1>
+              <div
+                style={{
+                  width: "100%",
+                  display: "flex",
+                  justifyContent: "center",
+                  alignItems: "center",
+                }}
+              >
+                <div style={{ width: "fit-content" }}>
+                  <Button
+                    btnText="Go Back"
+                    btnLink="/"
+                    btnType="secondary"
+                    glow={true}
+                  />
+                </div>
+              </div>
+            </>
           )}
         </section>
       </div>
