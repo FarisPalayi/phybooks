005107e0c2f9d7265e88b6dc53a4aab57932f48f
FarisPalayi <farispalayi@gmail.com>
2024-03-05 16:21:53 +0530

    update Button to be used in nav

--- src/app/components/shared/Button.js
+++ src/app/components/shared/Button.js
@@ -1,15 +1,19 @@
 import styles from "../../styles/components/Button.module.scss";
+import Link from "next/link";
 
-export default function Button({ btnText, btnType, btnLink, glow: shadow = false }) {
-  !["primary", "secondary"].includes(btnType) && (btnType = "secondary");
-  const shadowClass = shadow ? styles.shadow : styles.notShadow;
-
-  return (
-    <a
-      href={btnLink}
-      className={`${styles.btn} ${styles[btnType]} ${shadowClass}`}
-    >
-      {shadow && (
+export default function Button({
+  btnText,
+  btnType,
+  btnLink,
+  glow = false,
+  isNav = false,
+}) {
+  // Ensure btnType is either "primary" or "secondary"
+  btnType = ["primary", "secondary"].includes(btnType) ? btnType : "secondary";
+  const shadowClass = glow ? styles.shadow : styles.notShadow;
+  const contentJSX = (
+    <>
+      {glow && (
         <>
           <span className={`${styles.gradShadow} ${styles.grad1}`}></span>
           <span className={`${styles.gradShadow} ${styles.grad2}`}></span>
@@ -17,6 +21,24 @@ export default function Button({ btnText, btnType, btnLink, glow: shadow = false
         </>
       )}
       <span className={styles.content}>{btnText}</span>
+    </>
+  );
+
+  const ButtonElement = isNav ? (
+    <Link
+      href={btnLink}
+      className={`${styles.btn} ${styles[btnType]} ${shadowClass}`}
+    >
+      {contentJSX}
+    </Link>
+  ) : (
+    <a
+      href={btnLink}
+      className={`${styles.btn} ${styles[btnType]} ${shadowClass}`}
+    >
+      {contentJSX}
     </a>
   );
+
+  return ButtonElement;
 }
