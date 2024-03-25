c1311281fcd8688826e540b325c0f5af8a08e324
FarisPalayi <farispalayi@gmail.com>
2024-03-25 13:42:57 +0530

    swipe + next, prev btns working (index lag)

--- src/app/components/shared/ReadOnline.js
+++ src/app/components/shared/ReadOnline.js
@@ -1,6 +1,6 @@
 "use client";
 
-import { useRef, useCallback, useState, useEffect } from "react";
+import { useRef, useCallback, useState } from "react";
 import { useResizeObserver } from "@wojtekmaj/react-hooks";
 import { Document, Page, pdfjs, Outline } from "react-pdf";
 import "react-pdf/dist/esm/Page/AnnotationLayer.css";
@@ -16,11 +16,6 @@ import "swiper/css/navigation";
 
 pdfjs.GlobalWorkerOptions.workerSrc = `//unpkg.com/pdfjs-dist@${pdfjs.version}/build/pdf.worker.min.js`;
 
-const options = {
-  cMapUrl: "/cmaps/",
-  standardFontDataUrl: "/standard_fonts/",
-};
-
 const resizeObserverOptions = {};
 
 const maxWidth = 400;
@@ -30,15 +25,7 @@ export default function ReadOnline({ file }) {
   const [pageNumber, setPageNumber] = useState(1);
   const [containerRef, setContainerRef] = useState(null);
   const [containerWidth, setContainerWidth] = useState();
-  const [swiperReady, setSwiperReady] = useState(false);
-  const swiperRef = useRef(null);
-
-  useEffect(() => {
-    if (swiperRef.current) {
-      // Swiper instance is available, set swiperReady to true
-      setSwiperReady(true);
-    }
-  }, [swiperRef]);
+  const [swiperRef, setSwiperRef] = useState(null);
 
   const pageWidth = containerWidth
     ? Math.min(containerWidth, maxWidth)
@@ -47,9 +34,7 @@ export default function ReadOnline({ file }) {
   const onResize = useCallback((entries) => {
     const [entry] = entries;
 
-    if (entry) {
-      setContainerWidth(entry.contentRect.width);
-    }
+    if (entry) setContainerWidth(entry.contentRect.width);
   }, []);
 
   useResizeObserver(containerRef, resizeObserverOptions, onResize);
@@ -61,6 +46,11 @@ export default function ReadOnline({ file }) {
 
   // navigation
 
+  const slideTo = (index) => {
+    setPageNumber(index);
+    swiperRef.slideTo(index - 1, 0);
+  };
+
   const updateSwiperIndex = (index) => {
     if (swiperRef.current) {
       swiperRef.current.swiper.activeIndex = index;
@@ -69,77 +59,65 @@ export default function ReadOnline({ file }) {
   };
 
   const changePage = (offset) => {
-    const newPage = pageNumber + offset;
-    if (newPage >= 1 && newPage <= numPages) {
-      let newPageIndex = null;
-
-      setPageNumber((prevPageNumber) => {
-        newPageIndex = prevPageNumber + offset;
-        return newPageIndex;
-      });
-
-      updateSwiperIndex(newPageIndex - 1);
-
-      if (swiperReady) swiperRef.current.slideTo(newPage - 1); //! doesn't work
-    }
+    setPageNumber((prevPageNumber) => prevPageNumber + offset);
+    slideTo(pageNumber + offset);
   };
 
   const previousPage = () => changePage(-1);
   const nextPage = () => changePage(1);
 
-  const onItemClick = ({ pageNumber: itemPageNumber }) => {
-    setPageNumber(itemPageNumber);
-    updateSwiperIndex(itemPageNumber - 1);
+  const onIndexNav = ({ pageNumber: indexPageNumber }) => {
+    setPageNumber(indexPageNumber);
+    slideTo(indexPageNumber);
+  };
 
-    if (swiperReady) swiperRef.current.slideTo(itemPageNumber - 1); //! doesn't work
+  const handleSlideChange = (swiper) => {
+    setPageNumber(swiper.activeIndex + 1);
   };
 
   return (
-    <article>
+    <div>
       <div className={styles.documentContainer} ref={setContainerRef}>
         <Document
           file={file}
           onLoadSuccess={onDocumentLoadSuccess}
-          options={options}
           className={styles.document}
         >
           <div className={styles.TOC}>
             <h2 className={styles.index__title}>Index</h2>
-            <Outline onItemClick={onItemClick} className={styles.index} />
+            <Outline onItemClick={onIndexNav} className={styles.index} />
           </div>
 
           <div>
-
             <div
               className={styles.page__backdrop}
               style={{ width: pageWidth, height: pageWidth * 1.41 }}
             >
               <Swiper
                 ref={swiperRef}
-                onSlideChange={(swiper) =>
-                  setPageNumber(swiper.activeIndex + 1)
-                }
-                zoom={true}
                 navigation={true}
+                onSlideChange={handleSlideChange}
                 style={{
                   "--swiper-navigation-color": "#fff",
                   "--swiper-pagination-color": "var(--cyan)",
+                }}
+                zoom={true}
+                keyboard={{
+                  enabled: true,
                 }}
                 pagination={{
                   type: "progressbar",
                   clickable: true,
                 }}
-                keyboard={{
-                  enabled: true,
-                }}
-                modules={[Keyboard, Zoom, Navigation, Pagination]}
+                modules={[Zoom, Navigation, Pagination, Keyboard]}
                 className="mySwiper"
+                onSwiper={setSwiperRef}
               >
                 {[...Array(numPages).keys()].map((pageIndex) => (
                   <SwiperSlide key={pageIndex}>
                     {/* {pageIndex + 1 === pageNumber && ( */}
                     <Page
-                      pageNumber={pageNumber || 1}
+                      pageNumber={pageIndex + 1}
                       width={pageWidth}
                       className={styles.page}
                     />
@@ -153,6 +131,8 @@ export default function ReadOnline({ file }) {
               <p className={styles.pageNumber}>
                 Page {pageNumber || (numPages ? 1 : "--")} of {numPages || "--"}
               </p>
+
+              <button onClick={() => slideTo(15)}>jump to 15</button>
 
               <div className={styles.navBtns}>
                 <button
@@ -176,6 +156,6 @@ export default function ReadOnline({ file }) {
           </div>
         </Document>
       </div>
-    </article>
+    </div>
   );
 }
