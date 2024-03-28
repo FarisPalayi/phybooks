178fb3858e0b032a566f5aabb67675f4fe88f8c2
FarisPalayi <farispalayi@gmail.com>
2024-03-28 21:59:08 +0530

    feature: show more btn in subject grid

--- src/app/components/home/Subject.js
+++ src/app/components/home/Subject.js
@@ -1,10 +1,35 @@
+"use client";
+
 import styles from "../../styles/components/Subject.module.scss";
 import LinkCell from "../shared/LinkCell";
-import { getBookTitles, getBooksBySubject } from "@/app/lib/utils";
+import { getBookTitles } from "@/app/lib/utils";
 import { textbooks } from "@/app/lib/data";
+import { useEffect, useState } from "react";
 
 export default function Subject() {
+  const [windowWidth, setWindowWidth] = useState(window.innerWidth);
+  const [initialMaxBooks, setInitialMaxBooks] = useState(5);
+  const [maxBooks, setMaxBooks] = useState(initialMaxBooks);
   const bookTitles = getBookTitles(textbooks.sortBooksByTitle());
+  const [isExpanded, setIsExpanded] = useState(false);
+
+  const breakpoint = 600;
+
+  useEffect(() => {
+    const handleWindowResize = () => {
+      setInitialMaxBooks(windowWidth > breakpoint ? 9 : 5);
+      setWindowWidth(window.innerWidth);
+    };
+    window.addEventListener("resize", handleWindowResize);
+    return () => window.removeEventListener("resize", handleWindowResize);
+  }, [windowWidth]);
+
+  const handleShow = () => {
+    setIsExpanded(!isExpanded);
+    setMaxBooks((currentMaxBooks) =>
+      currentMaxBooks <= initialMaxBooks ? bookTitles.length : initialMaxBooks
+    );
+  };
 
   return (
     <section className={styles.subject} id="subject">
@@ -12,7 +37,7 @@ export default function Subject() {
         Browse by <span>Subject</span>
       </h2>
       <div className={styles.subject__grid}>
-        {bookTitles.map((title, i) => (
+        {bookTitles.slice(0, maxBooks).map((title, i) => (
           <LinkCell
             link={`/subject/${title}`}
             key={i}
@@ -21,7 +46,12 @@ export default function Subject() {
           />
         ))}
       </div>
-      {/* <button className={styles.showMore}>show more</button> */}
+
+      {bookTitles.length > initialMaxBooks && (
+        <button className={styles.showMore} onClick={handleShow}>
+          show {isExpanded ? "less" : "more"}
+        </button>
+      )}
     </section>
   );
 }
