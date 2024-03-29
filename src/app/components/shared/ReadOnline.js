b0d43b5ee255e4a6e90b942f226d6f6d62dc3629
FarisPalayi <farispalayi@gmail.com>
2024-03-30 02:02:29 +0530

    fix:

--- src/app/components/shared/ReadOnline.js
+++ src/app/components/shared/ReadOnline.js
@@ -102,7 +102,7 @@ export default function ReadOnline({ file, isFullView }) {
 
   return (
     <div className={styles.documentContainer} ref={setContainerRef}>
-      {true && <SkeletonReader />}
+      {isLoading && <SkeletonReader />}
 
       <Document
         file={file}
