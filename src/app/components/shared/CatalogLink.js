bb5822c45453ccbbf163484fea50fb1cb6b4403f
FarisPalayi <farispalayi@gmail.com>
2024-03-30 17:48:30 +0530

    add toast to catalog

--- src/app/components/shared/CatalogLink.js
+++ src/app/components/shared/CatalogLink.js
@@ -1,12 +1,28 @@
+"use client";
+
 import styles from "../../styles/components/CatalogLink.module.scss";
 import Link from "next/link";
 import ChevronRightIcon from "../icons/ChevronRightIcon";
 import BookIcon from "../icons/BookIcon";
+import Toast from "./Toast";
+import { useEffect, useState } from "react";
 
 export default function CatalogLink() {
+  const [showToast, setShowToast] = useState(false);
+  const closeDelay = 3000;
+
+  useEffect(() => {
+    if (!showToast) return;
+    const timer = setTimeout(() => setShowToast(false), closeDelay);
+    return () => clearTimeout(timer);
+  }, [showToast]);
+
   return (
     <div className={styles.catalog}>
-      <Link href="/catalog" className={styles.catalog__link}>
+      <button
+        className={styles.catalog__link}
+        onClick={() => setShowToast(true)}
+      >
         <span className={styles.bookIcon}>
           <BookIcon />
         </span>
@@ -14,7 +30,15 @@ export default function CatalogLink() {
         <span className={styles.nextIcon}>
           <ChevronRightIcon />
         </span>
-      </Link>
+      </button>
+      {showToast && (
+        <Toast
+          info="Page is under construction!"
+          state="Warning"
+          show={showToast}
+          onClose={() => setShowToast(false)}
+        />
+      )}
     </div>
   );
 }
