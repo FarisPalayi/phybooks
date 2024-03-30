d1b76c39f2d59d4387711150e10b7e4f7f8cbfc2
FarisPalayi <farispalayi@gmail.com>
2024-03-30 17:34:16 +0530

    add sidebar links

--- src/app/components/shared/Sidebar.js
+++ src/app/components/shared/Sidebar.js
@@ -3,56 +3,54 @@ import styles from "../../styles/components/Sidebar.module.scss";
 
 export default function Sidebar({ isSidebarOpen }) {
   const navItems = [
-    "Home",
-    // {
-    //   Textbooks: [
-    //     "Semester 1",
-    //     "Semester 2",
-    //     "Semester 3",
-    //     "Semester 4",
-    //     "Semester 5",
-    //     "Semester 6",
-    //   ],
-    // },
-    "Reference Books",
-    // "Favorites",
+    ["Home", "/"],
+    // ["Reference Books", "/reference-books"],
+    // ["Favorites", "/favorites"],
     // { Settings: ["Theme", "Mode"] },
-    "Source Code",
-    "Design Files",
-    "About",
-    "Contact",
+    [
+      "Design Files",
+      "https://www.figma.com/file/xmcgV2La7Y4FOmmROXjNka/Bsc-physics?type=design&node-id=0%3A1&mode=design&t=aIAHqQmsHp7nal0A-1",
+    ],
+    ["Source Code", "https://github.com/farispalayi/phybooks"],
+    ["About", "/about"],
+    ["Contact", "/contact"],
   ];
 
   const renderNavItems = (navItems) => {
     return navItems.map((item, index) => {
-      if (typeof item === "string") {
+      if (typeof item[0] === "string") {
         return (
           <li key={index} className={styles.nav__item}>
-            <Link href="#">{item}</Link>
+            <Link
+              href={item[1]}
+              target={item[1].startsWith("http") ? "_blank" : "_self"}
+            >
+              {item[0]}
+            </Link>
           </li>
         );
       }
 
-      if (typeof item === "object") {
-        if (Object.keys(item).length > 1)
-          throw new Error("Sidebar: Object must have only one key");
+      // if (typeof item === "object") {
+      //   if (Object.keys(item).length > 1)
+      //     throw new Error("Sidebar: Object must have only one key");
 
-        const navItem = Object.keys(item)[0];
-        const navItemList = item[navItem];
+      //   const navItem = Object.keys(item)[0];
+      //   const navItemList = item[navItem];
 
-        return (
-          <li key={index} className={styles.nav__item}>
-            <button className={styles.nav__btn}>{navItem}</button>
-            <ul className={styles.nav__sublist}>
-              {navItemList.map((subItem, subIndex) => (
-                <li key={subIndex} className={styles.nav__subitem}>
-                  <Link href="#">{subItem}</Link>
-                </li>
-              ))}
-            </ul>
-          </li>
-        );
-      }
+      //   return (
+      //     <li key={index} className={styles.nav__item}>
+      //       <button className={styles.nav__btn}>{navItem}</button>
+      //       <ul className={styles.nav__sublist}>
+      //         {navItemList.map((subItem, subIndex) => (
+      //           <li key={subIndex} className={styles.nav__subitem}>
+      //             <Link href="#">{subItem}</Link>
+      //           </li>
+      //         ))}
+      //       </ul>
+      //     </li>
+      //   );
+      // }
     });
   };
 
