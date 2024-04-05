d9d6f2eb9dd64aa9fac97b90391b4cf87f41dc9f
FarisPalayi <farispalayi@gmail.com>
2024-04-06 04:40:18 +0530

    add astrophysics reference book

--- src/app/lib/data.js
+++ src/app/lib/data.js
@@ -126,6 +126,25 @@ const textbooks = [
     pages: 566,
     size: 22,
   },
+  {
+    id: 8,
+    title: "Astrophysics Is Easy!: An Introduction for the Amateur Astronomer",
+    author: "Michael Inglis",
+    publishedDate: "2023",
+    semester: 6,
+    subject: ["relativistic mechanics and astrophysics"],
+    filepath:
+      "/textbooks/semester-6/reference-books/Astrophysics Is Easy! An Introduction for the Amateur Astronomer.pdf",
+    image: {
+      url: "/textbooks/semester-6/reference-books/covers/astrophysics-is-easy.jpg",
+      width: 126,
+      height: 191.5,
+    },
+    isReference: true,
+    category: [],
+    pages: 462,
+    size: 14,
+  },
 ];
 
 export { textbooks };
